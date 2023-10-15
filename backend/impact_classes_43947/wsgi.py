7599b599a8d875235c34c07ab02177d716c8607c
Crowdbotics <team@crowdbotics.com>
2023-10-15 00:18:26 +0000

    Initial commit

---
+++ backend/impact_classes_43947/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for impact_classes_43947 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "impact_classes_43947.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "impact-classes-43947",
+  url: "https://impact-classes-43947.botics.co",
+  api: "https://impact-classes-43947.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
